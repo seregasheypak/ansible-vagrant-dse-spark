4a964e13c4666a88dd91c43c1e24b266028d458a
ssa <serega.sheypak@gmail.com>
2015-04-13 14:17:00 +0200

    + spark-job-history server

---
+++ start-spark-history-server.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+ansible-playbook \
+-i .vagrant/provisioners/ansible/inventory/vagrant_ansible_inventory \
+--private-key=.vagrant/machines/dsenode01/virtualbox/private_key \
+-u vagrant \
+playbook.yml \
+--tags "start-spark-history"
