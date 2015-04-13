9d76de1e261a1d4160ed4f7cb08370804aceb863
ssa <serega.sheypak@gmail.com>
2015-04-13 15:02:55 +0200

    fix misprint, it's on dsenode02

--- start-spark-jobserver.sh
+++ start-spark-jobserver.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 ansible-playbook \
 -i .vagrant/provisioners/ansible/inventory/vagrant_ansible_inventory \
---private-key=.vagrant/machines/dsenode01/virtualbox/private_key \
+--private-key=.vagrant/machines/dsenode02/virtualbox/private_key \
 -u vagrant \
 playbook.yml \
 --tags "start-spark-jobserver"
