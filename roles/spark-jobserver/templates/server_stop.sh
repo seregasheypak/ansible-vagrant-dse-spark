64996a375193a4d0c6e8927dfafbd97a40a2ec54
ssa <serega.sheypak@gmail.com>
2015-04-27 18:16:16 +0200

    kill using parent pid

--- roles/spark-jobserver/templates/server_stop.sh
+++ roles/spark-jobserver/templates/server_stop.sh
@@ -6,7 +6,8 @@ PIDFILE="/var/run/spark-jobserver.pid"
 echo "got pid frmo file: " ${PIDFILE} " pid value:" $(cat "$PIDFILE")
 
 echo "server_stop:02"
-if [ ! -f "$PIDFILE" ] || ! kill -9 $(cat "$PIDFILE"); then
+#if [ ! -f "$PIDFILE" ] || ! kill -9 $(cat "$PIDFILE"); then
+if [ ! -f "$PIDFILE" ] || ! pkill -TERM -P $(cat "$PIDFILE"); then
    echo 'Job server not running'
 else
   echo 'Stopping job server...'
